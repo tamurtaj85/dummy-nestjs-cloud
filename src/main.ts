f763b09c37e2336de2fd5579806833fbb3c3194e
Tamur Ahmed <tamurahmed@Tamurs-MacBook-Air.local>
2024-10-25 14:21:27 +0500

    added-logs

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,10 @@ const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port, '0.0.0.0');
+
+  const appUrl = await app.getUrl();
+
+  console.log(`Server available at ${appUrl}`);
 }
+
 bootstrap();
