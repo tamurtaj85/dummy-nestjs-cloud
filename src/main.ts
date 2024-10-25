c27c91f0111f0e4ff82cfd6ed644184a9c7790fc
Tamur Ahmed <tamurahmed@Tamurs-MacBook-Air.local>
2024-10-25 15:00:58 +0500

    installed config package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   await app.listen(port, '0.0.0.0');
 
   const appUrl = await app.getUrl();
